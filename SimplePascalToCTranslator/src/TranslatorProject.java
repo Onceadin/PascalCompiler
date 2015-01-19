799bc1faf8d33b1c8928173f2e54f44d031d8aa7
mtl14 <mentallmail@gmail.com>
2015-01-19 12:15:19 +0300

    renamed

--- SimplePascalToCTranslator/src/TranslatorProject.java
+++ SimplePascalToCTranslator/src/TranslatorProject.java
@@ -3,7 +3,7 @@ import org.antlr.v4.runtime.tree.*;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
-public class KALKULATORSUKA {
+public class TranslatorProject {
 	
 	public static void main(String[] args) throws Exception {
 		String inputFile = null;
